dffe2a3cc94c6ed5fa2dedad5cff9d1fcc3642d9
Matteo Giaccone <2846623+mgiaccone@users.noreply.github.com>
2022-07-08 11:36:05 +0100

    chore(workflow): test release

--- .github/scripts/releasecheck.sh
+++ .github/scripts/releasecheck.sh
@@ -2,13 +2,11 @@
 
 echo "::group::releasecheck"
 
-echo "::debug::Checking for releasable file changes"
+echo "::debug::Checking diff with previous commit for releasable file changes"
 release=$(git diff --name-only ${INPUT_BASE_SHA} ${INPUT_SHA} | grep -cE '[^\.].*\.(go|mod|sum|md)')
 
 if [ ${release} -gt 0 ]; then
-  echo "::debug::Changes detected"
   echo "::set-output name=changed::true"
 else
-  echo "::debug::No changes detected"
   echo "::set-output name=changed::false"
 fi
