95ed534e4da9a70ddd64b8ec611e6ab9d6599347
Matteo Giaccone <2846623+mgiaccone@users.noreply.github.com>
2022-07-08 12:12:41 +0100

    chore(workflow): test release

--- .github/scripts/releasecheck.sh
+++ .github/scripts/releasecheck.sh
@@ -2,17 +2,20 @@
 
 echo "::echo::on"
 
-echo "::debug::INPUT_SHA ${INPUT_SHA}"
-echo "::debug::INPUT_BASE_SHA ${INPUT_BASE_SHA}"
+current_hash=$(git rev-list --no-merges -n 1 HEAD)
+previous_hash=$(git rev-list --no-merges -n 1 HEAD^)
+
+echo "::debug::Current commit: ${current_hash}"
+echo "::debug::Previous commit: ${previous_hash}"
 
 echo "::debug::Commit log"
 git --no-pager log --decorate=short --pretty=oneline -n2
 
 echo "::debug::Commit diff"
-git diff --name-only ${INPUT_BASE_SHA} ${INPUT_SHA}
+git diff --name-only ${previous_hash} ${current_hash}
 
 echo "::debug::Checking diff with previous commit for releasable file changes"
-release=$(git diff --name-only ${INPUT_BASE_SHA} ${INPUT_SHA} | grep -cE '[^\.].*\.(go|mod|sum|md)')
+release=$(git diff --name-only ${previous_hash} ${current_hash} | grep -cE '[^\.].*\.(go|mod|sum|md)')
 
 if [ ${release} -gt 0 ]; then
   echo "::set-output name=changed::true"
