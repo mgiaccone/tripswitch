60a9d6361f16c6110c7e21227fe8c656570b59c6
Matteo Giaccone <2846623+mgiaccone@users.noreply.github.com>
2022-07-08 12:16:25 +0100

    chore(workflow): test release

--- .github/scripts/releasecheck.sh
+++ .github/scripts/releasecheck.sh
@@ -5,18 +5,19 @@ echo "::echo::on"
 current_hash=$(git rev-list --no-merges -n 1 HEAD)
 previous_hash=$(git rev-list --no-merges -n 1 HEAD^)
 
-echo "::debug::Current commit: ${current_hash}"
-echo "::debug::Previous commit: ${previous_hash}"
+echo "Current commit: ${current_hash}"
+echo "Previous commit: ${previous_hash}"
 
-echo "::debug::Commit log"
+echo "Commit log"
 git --no-pager log --decorate=short --pretty=oneline -n2
 
-echo "::debug::Commit diff"
+echo "Changed files list"
 git diff --name-only ${previous_hash} ${current_hash}
 
-echo "::debug::Checking diff with previous commit for releasable file changes"
+echo "Checking diff with previous commit for releasable file changes"
 release=$(git diff --name-only ${previous_hash} ${current_hash} | grep -cE '[^\.].*\.(go|mod|sum|md)')
 
+echo "Setting check result"
 if [ ${release} -gt 0 ]; then
   echo "::set-output name=changed::true"
 else
