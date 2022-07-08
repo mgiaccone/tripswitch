52790c754d3dca7c553ca22d3562df1b81f3e2ea
Matteo Giaccone <2846623+mgiaccone@users.noreply.github.com>
2022-07-08 11:41:11 +0100

    chore(workflow): test release

--- .github/scripts/releasecheck.sh
+++ .github/scripts/releasecheck.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-echo "::group::releasecheck"
+echo "::echo::on"
+
+git --no-pager log --decorate=short --pretty=oneline -n5
+git diff --name-only ${INPUT_BASE_SHA} ${INPUT_SHA}
 
 echo "::debug::Checking diff with previous commit for releasable file changes"
 release=$(git diff --name-only ${INPUT_BASE_SHA} ${INPUT_SHA} | grep -cE '[^\.].*\.(go|mod|sum|md)')
